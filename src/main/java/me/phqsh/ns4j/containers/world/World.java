6a9444181373d3c18687b53b291d71efa04fe4b5
Fishy <phishyemperor@gmail.com>
2023-01-10 16:36:56 -0500

    Add region shard to world

--- src/main/java/me/phqsh/ns4j/containers/world/World.java
+++ src/main/java/me/phqsh/ns4j/containers/world/World.java
@@ -6,10 +6,14 @@ import me.phqsh.ns4j.containers.world.shards.Faction;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "WORLD") @XmlAccessorType(XmlAccessType.FIELD)
 public class World implements Container {
-    @Getter
-    private Faction FACTION;
+    @Getter @XmlElement(name = "FACTION")
+    private Faction faction;
+
+    @Getter @XmlElement(name = "REGIONS")
+    private String regions;
 }
