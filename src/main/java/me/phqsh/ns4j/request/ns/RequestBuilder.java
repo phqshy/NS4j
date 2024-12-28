8a0f32d02c610404ceb401d1a98a690047f4b52c
phqshy <87027056+phqshy@users.noreply.github.com>
2024-12-28 16:27:02 -0500

    Update RequestBuilder
    Signed-off-by: phqshy <87027056+phqshy@users.noreply.github.com>

--- src/main/java/me/phqsh/ns4j/request/ns/RequestBuilder.java
+++ src/main/java/me/phqsh/ns4j/request/ns/RequestBuilder.java
@@ -4,6 +4,7 @@ import me.phqsh.ns4j.NationStatesAPI;
 import me.phqsh.ns4j.containers.Container;
 import me.phqsh.ns4j.containers.ContainerType;
 import me.phqsh.ns4j.containers.TargetedContainer;
+import me.phqsh.ns4j.containers.cards.Card;
 import me.phqsh.ns4j.enums.Options;
 import me.phqsh.ns4j.enums.Shards;
 import me.phqsh.ns4j.enums.wa.Council;
@@ -14,7 +15,7 @@ import me.phqsh.ns4j.request.http.HttpRequestImpl;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
-public class NationStatesRequestBuilder {
+public class RequestBuilder {
     private static final String BASE_URL = "https://www.nationstates.net/cgi-bin/api.cgi?";
     private Class<?> decodingClass;
     private List<Shards> shards = new ArrayList<>();
@@ -23,99 +24,111 @@ public class NationStatesRequestBuilder {
     private String waResolution;
     private String url;
 
-    public NationStatesRequestBuilder setType(Class<?> clazz) {
+    public RequestBuilder setType(Class<?> clazz) {
+        // decoding types must extend Container
         Class<?> superClass = clazz.getSuperclass();
         if (superClass != Container.class) {
-            throw new RequestBuilderException("Class " + clazz.getSimpleName() + " does not derive from " + Container.class.getSimpleName() + ".");
+            throw new RequestBuilderException("Class " + clazz.getSimpleName() + " does not extend " + Container.class.getSimpleName() + ".");
         }
         this.decodingClass = clazz;
         return this;
     }
 
-    public NationStatesRequestBuilder addShards(Shards... shards) {
+    public RequestBuilder addShards(Shards... shards) {
         this.shards.addAll(List.of(shards));
         return this;
     }
 
-    public NationStatesRequestBuilder clearShards() {
+    public RequestBuilder clearShards() {
         this.shards = new ArrayList<>();
         return this;
     }
 
-    public NationStatesRequestBuilder addOptions(Map<String, String> map) {
+    public RequestBuilder addOptions(Map<String, String> map) {
         this.options.putAll(map);
         return this;
     }
 
-    public NationStatesRequestBuilder addOption(String key, String value) {
+    public RequestBuilder addOption(String key, String value) {
         this.options.put(key, value);
         return this;
     }
 
-    public NationStatesRequestBuilder addOption(String key, int value) {
+    public RequestBuilder addOption(String key, int value) {
         return addOption(key, String.valueOf(value));
     }
 
-    public NationStatesRequestBuilder addOption(Options key, String value) {
+    public RequestBuilder addOption(Options key, String value) {
         return addOption(key.getId(), value);
     }
 
-    public NationStatesRequestBuilder addOption(Options key, int value) {
+    public RequestBuilder addOption(Options key, int value) {
         return addOption(key.getId(), value);
     }
 
-    public NationStatesRequestBuilder addOption(Options key, Options value) {
+    public RequestBuilder addOption(Options key, Options value) {
         return addOption(key.getId(), value.getId());
     }
 
-    public NationStatesRequestBuilder clearOptions() {
+    public RequestBuilder clearOptions() {
         this.options = new HashMap<>();
         return this;
     }
 
-    public NationStatesRequestBuilder setTarget(String target) {
+    public RequestBuilder setTarget(String target) {
         this.target = target.toLowerCase().replace(" ", "_");
         return this;
     }
 
-    public NationStatesRequestBuilder setTarget(int target) {
+    public RequestBuilder setTarget(int target) {
         this.target = String.valueOf(target);
         return this;
     }
 
-    public NationStatesRequestBuilder setTarget(Council target) {
+    public RequestBuilder setTarget(Council target) {
         this.target = String.valueOf(target.getId());
         return this;
     }
 
-    public NationStatesRequestBuilder setWaResolution(String id) {
+    public RequestBuilder setWaResolution(String id) {
         this.waResolution = id;
         return this;
     }
 
-    public NationStatesRequestBuilder setWaResolution(int id) {
+    public RequestBuilder setWaResolution(int id) {
         this.waResolution = String.valueOf(id);
         return this;
     }
 
     public String generateUrl() {
+        // initialize the base URL with appropriate prefixes
         StringBuilder url = new StringBuilder(BASE_URL);
         url.append(ContainerType.CLASSES.get(decodingClass)).append("=");
 
+        // targeted containers (ex nation where the url contains "nation=x" before list of shards)
         if (Arrays.stream(decodingClass.getInterfaces()).anyMatch(e -> e == TargetedContainer.class)) {
             if (target == null) {
                 throw new RequestBuilderException("You must specify a target when fetching this request!");
             }
 
-            url.append(target).append("&q=");
+            // prep for adding shards
+            url.append(target).append("&q=");;
+
+            // the "cards" shard is implied for the Cards container, so hardcode it here
+            if (decodingClass == Card.class) {
+                url.append("card+");
+            }
         }
 
+        // add shards to URL
         for (Shards i : shards){
             url = new StringBuilder(url.toString().concat(i.getId().concat("+")));
         }
 
+        // prep for additional options
         url.append(";");
 
+        // add options to URL
         for (String i : options.keySet()) {
             url.append(i).append("=").append(options.get(i)).append(";");
         }
@@ -123,7 +136,7 @@ public class NationStatesRequestBuilder {
         return url.toString();
     }
 
-    public NationStatesRequestBuilder setUrl(String url) {
+    public RequestBuilder setUrl(String url) {
         this.url = url;
         return this;
     }
